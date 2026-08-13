723cdedee8e9fac28ee5d21754bbc6662b7e5461
jawad-ahmed1997 <jawad.walbi@gmail.com>
2026-08-13 22:30:21 +0500

    Fix team Lead Roll error

--- src/app/users/layout.js
+++ src/app/users/layout.js
@@ -1,2 +1,2 @@
 import { requireRole } from "@/lib/auth/authorization";
-export default async function UsersLayout({ children }) { await requireRole(["CEO", "PM", "CTO"]); return children; }
+export default async function UsersLayout({ children }) { await requireRole(["CEO", "PM", "CTO", "TEAM_LEAD"]); return children; }
